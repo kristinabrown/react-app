c1670e255377d5a3a3245881860d979e81ff134e
Kristina Brown <kristina.frey.frey@gmail.com>
2016-12-15 10:14:10 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    let txt = this.props.txt
+    return (
+      <div>
+        <h1>{txt}</h1>
+        <b>bold</b>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "This is the default text"
+}
+
+
+export default App
