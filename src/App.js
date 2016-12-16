cb94dda4ecd0c2e014eef07ffd0e95c04e69badc
Kristina Brown <kristina.frey.frey@gmail.com>
2016-12-16 11:30:42 -0700

    add state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: "this is the state txt",
+      cat: 0
+    }
+  }
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
   render(){
     let txt = this.props.txt
     return (
       <div>
-        <h1>{txt}</h1>
-        <b>bold</b>
+        <input type='text' onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
       </div>
     )
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "This is the default text"
-}
+// props are meant to remain static
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: "This is the default text"
+// }
 
 
 export default App
