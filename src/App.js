d00bf8e0570c9ce45d6e71c0f6b15116006c6522
Kristina Brown <kristina.frey.frey@gmail.com>
2016-12-20 11:07:46 -0700

    events

--- src/App.js
+++ src/App.js
@@ -3,57 +3,29 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      txt: "this is the state txt",
-      cat: 0
-    }
+    this.state = {currentEvent: '----'}
+    this.update = this.update.bind(this)
   }
-  updateText(e) {
-    this.setState({txt: e.target.value})
+
+  update(e) {
+    this.setState({currentEvent: e.type})
   }
-  render(){
+  render() {
     return (
-      <Title text="the text"/>
-      // <Button>I <Heart/> React</Button>
-      // <div>
-      //   <h1>{this.state.txt} - {this.state.cat}</h1>
-      //   <Widget updateText={this.updateText.bind(this)}/>
-      // </div>
+      <div>
+        <textarea
+        onKeyPress={this.update}
+        onCopy={this.update}
+        cols="30"
+        rows="10"/>
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+
     )
   }
 }
 
-const Title = (props) => <h1>Title: {props.text}</h1>
 
-Title.propTypes = {
-  text(props, propName, component){
-    if(!(propName in props)){
-      return new Error(`missing ${propName}`)
-    }
-    if(props[propName].length < 6){
-      return new Error(`${propName} was too short`)
-    }
-  }
-}
-// const Button = (props) => <button>{props.children}</button>
-//
-// class Heart extends React.Component {
-//   render() {
-//     return <span>&hearts;</span>
-//   }
-// }
-
-// const Widget = (props) =>
-//   <input type='text' onChange={props.updateText}/>
-// props are meant to remain static
-// App.propTypes = {
-//   txt: React.PropTypes.string,
-//   cat: React.PropTypes.number.isRequired
-// }
-//
-// App.defaultProps = {
-//   txt: "This is the default text"
-// }
 
 
 export default App
