359308837b81bed84c8ce18a1f8638fc1d8fd28c
Kristina Brown <kristina.frey.frey@gmail.com>
2016-12-20 11:32:43 -0700

    referencing components

--- src/App.js
+++ src/App.js
@@ -3,29 +3,38 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {currentEvent: '----'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
-
-  update(e) {
-    this.setState({currentEvent: e.type})
+  update() {
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.b.refs.input.value
+    })
   }
   render() {
     return (
       <div>
-        <textarea
-        onKeyPress={this.update}
-        onCopy={this.update}
-        cols="30"
-        rows="10"/>
-        <h1>{this.state.currentEvent}</h1>
+        <Input
+          ref={component => this.a = component}
+          update={this.update.bind(this)}
+        />
+         {this.state.a}
+        <hr/>
+          <Input
+            ref={component => this.b = component}
+            update={this.update.bind(this)}
+          />
+           {this.state.b}
       </div>
-
     )
   }
+
 }
 
-
-
+class Input extends React.Component {
+  render() {
+    return <div><input ref="input" type="text" onChange={this.props.update}/></div>
+  }
+}
 
 export default App
